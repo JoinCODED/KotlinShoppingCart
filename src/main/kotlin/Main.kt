161faf339815ecf6c825b5d634cb2966fe25e46d
badermalshammari <q8.badoori.11@gmail.com>
2025-02-13 20:34:02 +0300

    Misk Aldukhi - Bader Alshammari

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,7 +11,36 @@ package shoppingCart
  *   5 -> { "name"="Wireless Mouse", "price"=8.500, "stock"=15 },
  */
 fun createStoreInventory(): MutableMap<Int, MutableMap<String, Any>> {
-    TODO("Implement createStoreInventory()")
+    val store: MutableMap<Int, MutableMap<String, Any>> = mutableMapOf(
+        1 to mutableMapOf(
+            "name" to "Laptop",
+            "price" to 350.000,
+            "stock" to 10
+        ),
+
+        2 to mutableMapOf(
+            "name" to "Smart TV",
+            "price" to 200.000,
+            "stock" to 5
+        ),
+
+        3 to mutableMapOf(
+            "name" to "Headphones",
+            "price" to 50.000,
+            "stock" to 20
+        ),
+        4 to mutableMapOf(
+            "name" to "Gaming Console",
+            "price" to 150.000,
+            "stock" to 8
+        ),
+        5 to mutableMapOf(
+            "name" to "Wireless Mouse",
+            "price" to 8.500,
+            "stock" to 15
+        ))
+    return store
+
 }
 
 /**
@@ -25,7 +54,22 @@ fun addToCart(
     productId: Int,
     quantity: Int
 ): Boolean {
-    TODO("Implement addToCart()")
+    val product = storeInventory[productId]
+
+    if (product != null) {
+        val productStock = product["stock"] as Int
+
+
+        if (productStock >= quantity && productStock != 0) {
+
+            cart[productId] = cart.getOrDefault(productId, 0) + quantity
+            product["stock"] = productStock - quantity
+            return true
+        } else {
+            return false
+        }
+    }
+    return false
 }
 
 /**
@@ -39,9 +83,32 @@ fun removeFromCart(
     productId: Int,
     quantity: Int
 ): Boolean {
-    TODO("Implement removeFromCart()")
-}
+    val product = storeInventory[productId]
 
+    if (product != null && cart.containsKey(productId)) {
+
+        val cartQuantity = cart[productId]!!
+
+        if (cartQuantity > quantity ) {
+
+            cart[productId] = cartQuantity - quantity
+            product["stock"] = (product["stock"] as Int) + quantity
+
+            return true
+
+        }
+        else if (cartQuantity == quantity) {
+            cart.remove(productId)
+            product["stock"] = (product["stock"] as Int) + quantity
+            return true
+        }
+        else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
 /**
  * Calculates the total cost of items in [cart].
  * - For each (productId -> quantity), retrieve the price from [storeInventory].
@@ -51,8 +118,21 @@ fun calculateTotal(
     storeInventory: MutableMap<Int, MutableMap<String, Any>>,
     cart: MutableMap<Int, Int>
 ): Double {
-    TODO("Implement calculateTotal()")
+    var totalCost = 0.0
+    if (cart.isEmpty()) {
+        return 0.0
+    } else {
+        for ((productId, quantity) in cart) {
+            val product = storeInventory[productId]
+            if (product != null) {
+                val price = product["price"] as Double
+                totalCost = totalCost + (price * quantity)
+            }
+        }
+        return totalCost
+    }
 }
+
 
 /**
  * Returns a list of product IDs whose "name" contains [keyword].
@@ -62,15 +142,24 @@ fun calculateTotal(
 fun filterProductsByName(
     storeInventory: MutableMap<Int, MutableMap<String, Any>>,
     keyword: String
-): List<Int> {
-    TODO("Implement filterProductsByName()")
-}
+    ): List<Int> {
+        val productID = mutableListOf<Int>()
+
+        for ((productId, product) in storeInventory) {
+            val name = product["name"] as String
+            if (name.lowercase().contains(keyword.lowercase())) {
+                productID.add(productId)
+            }
+        }
+        return productID
+    }
 
 /**
  * Entry point for the project.
  * Run the JUnit tests to see if you've implemented each function correctly.
  */
 fun main() {
+    println(createStoreInventory())
     println("Welcome to the Kotlin Store project!")
     println("All functions currently throw NotImplementedError.")
     println("Open each function and replace TODO(...) with your implementation.")
